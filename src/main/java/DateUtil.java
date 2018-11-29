0e6faaf5990f5a08c8da460fb3939d9573c10881
liusw <liusw@usoftchina.com>
2018-11-29 11:51:47 +0800

    feat:增加计算两个日期之间的天数 方法

--- src/main/java/DateUtil.java
+++ src/main/java/DateUtil.java
@@ -1,3 +1,6 @@
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+
 /**
  * 日期工具类
  *
@@ -5,4 +8,10 @@
  * @date 2018-11-29 11:32
  */
 public class DateUtil {
+
+    // Java8 实现计算两个日期之间的天数
+    public Long utilDate(LocalDate endDate) {
+        return LocalDate.now().until(endDate, ChronoUnit.DAYS);
+    }
+
 }
